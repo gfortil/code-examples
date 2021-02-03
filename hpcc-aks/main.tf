68b13d903060ca60e9ef0735f884c1addf60537e
Fortil, Godson <godson.fortil@lexisnexisrisk.com>
2021-02-03 01:14:15 -0500

    Upgrade to new HPCC version

--- hpcc-aks/main.tf
+++ hpcc-aks/main.tf
@@ -1,20 +1,25 @@
+variable "subscription_id" {
+  default = "ec0ba952-4ae9-4f69-b61c-4b96ff470038"
+}
+
 #############
 # Providers #
 #############
 
 provider "azurerm" {
   version = ">=2.0.0"
-  subscription_id = "example"
+  subscription_id = var.subscription_id
   features {}
 }
 
 provider "helm" {
-  alias = "aks"
-  kubernetes {
-    host                   = module.kubernetes.host
-    client_certificate     = base64decode(module.kubernetes.client_certificate)
-    client_key             = base64decode(module.kubernetes.client_key)
+alias = "aks"
+kubernetes {
+  host = module.kubernetes.host
+    client_certificate = base64decode(module.kubernetes.client_certificate)
+    client_key = base64decode(module.kubernetes.client_key)
     cluster_ca_certificate = base64decode(module.kubernetes.cluster_ca_certificate)
+    config_path = "kube_config"
   }
 }
 
@@ -24,11 +29,11 @@ provider "helm" {
 
 module "subscription" {
   source = "github.com/Azure-Terraform/terraform-azurerm-subscription-data.git?ref=v1.0.0"
-  subscription_id = "example"
+  subscription_id = var.subscription_id
 }
 
 module "rules" {
-  source = "git@github.com:openrba/python-azure-naming.git?ref=tf"
+  source = "../../python-azure-naming"
 }
 
 module "metadata"{
@@ -37,14 +42,13 @@ module "metadata"{
   naming_rules = module.rules.yaml
   
   market              = "us"
-  project             = "example"
+  project             = "test_by_godji"
   location            = "useast2"
-  sre_team            = "example"
-  cost_center         = "example"
-  environment         = "sandbox"
-  product_name        = "example"
-  business_unit       = "example"
-  product_group       = "example"
+  sre_team            = "hpcc_platform"
+  environment         = "dev"
+  product_name        = "tfe"
+  business_unit       = "hpccplat"
+  product_group       = "core"
   subscription_id     = module.subscription.output.subscription_id
   subscription_type   = "nonprod"
   resource_group_type = "app"
@@ -58,24 +62,23 @@ module "resource_group" {
   tags     = module.metadata.tags
 }
 
-module "app_reg" {
-  source = "github.com/Azure-Terraform/terraform-azuread-application-registration.git?ref=v1.0.0"
-
-  names    = module.metadata.names
-  tags     = module.metadata.tags
-}
-
 module "kubernetes" {
-  source = "github.com/Azure-Terraform/terraform-azurerm-kubernetes.git?ref=v1.0.1"
+  source = "github.com/Azure-Terraform/terraform-azurerm-kubernetes.git?ref=v1.2.1"
   
   location                 = module.metadata.location
   names                    = module.metadata.names
   tags                     = module.metadata.tags
-  kubernetes_version       = "1.18.2"
+  kubernetes_version       = "1.18.8"
   resource_group_name      = module.resource_group.name
-  service_principal_id     = module.app_reg.application_id
-  service_principal_name   = module.app_reg.service_principal_name
-  service_principal_secret = module.app_reg.service_principal_secret
+
+  default_node_pool_name                = "default"
+  default_node_pool_vm_size             = "Standard_D2s_v3"
+  default_node_pool_enable_auto_scaling = true
+  default_node_pool_node_min_count      = 1
+  default_node_pool_node_max_count      = 5
+  default_node_pool_availability_zones  = [1,2,3]
+
+  enable_kube_dashboard = true
 }
 
 ###############
@@ -85,23 +88,50 @@ module "kubernetes" {
 resource "helm_release" "hpcc" {
   provider    = helm.aks
 
-  name       = "mycluster"
+  name       = "my-hpcc-terra-cluster"
   namespace  = "default"
   repository = "https://hpcc-systems.github.io/helm-chart/"
   chart      = "hpcc"
+  version    = "7.12.18-rc1"
 
-  values = [
-    "${file("values.yaml")}"
-  ]
+  set {
+    name  = "global.image.version"
+    value = "7.12.24-rc1"
+  }
+
+  set {
+    name  = "storage.dllStorage.storageClass"
+    value = "azurefile"
+  }
+
+  set {
+    name  = "storage.daliStorage.storageClass"
+    value = "azurefile"
+  }
+
+  set {
+    name  = "storage.dataStorage.storageClass"
+    value = "azurefile"
+  }
+
 }
 
 ##########
 # Output #
 ##########
+
 output "resource_group_name" {
   value = module.resource_group.name
 }
 
 output "aks_cluster_name" {
   value = module.kubernetes.name
+}
+
+output "aks_login" {
+  value = "az aks get-credentials --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
+}
+
+output "aks_browse"{
+  value = "az aks browse --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
 }
